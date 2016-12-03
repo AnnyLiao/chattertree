1f20df41868d9f3be2f3fbeb3ad273e13f9dd8cb
廖莉庭 <anny10144@gmail.com>
2016-12-03 22:02:42 +0800

    modify navbar

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(window).load(function () {
-
-    // preloader
+	
+	// preloader
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(550).css({
